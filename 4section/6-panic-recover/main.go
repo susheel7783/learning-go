1f94f060300eaf97b4063c8f98eb14f5bed3a8e8
Susheel Kumar <97793079+susheel7783@users.noreply.github.com>
2026-04-19 12:49:41 +0530

    Update main.go

--- 4section/6-panic-recover/main.go
+++ 4section/6-panic-recover/main.go
@@ -4,6 +4,9 @@ import "fmt"
 
 func mightPanic(shouldPanic bool) {
 	if shouldPanic {
+		// 1. THE PANIC
+		// When panic is called, the current function stops immediately.
+		// Go then starts "unwinding" the stack, running any 'defer' functions it finds.
 		panic("something went wrong in mightPanic")
 	}
 
@@ -11,17 +14,27 @@ func mightPanic(shouldPanic bool) {
 }
 
 func recoverable() {
+	// 2. THE RECOVER (Inside a Defer)
+	// 'recover' ONLY works when called inside a deferred function.
+	// It's like catching a falling object before it hits the ground.
 	defer func() {
+		// r will be nil if there was no panic.
+		// If there WAS a panic, r will contain the message from panic().
 		if r := recover(); r != nil {
 			fmt.Println("Recovered from panic:", r)
 		}
 	}()
 
+	// This call will trigger the panic logic.
 	mightPanic(true)
+	
+	// This line will NEVER run because mightPanic stopped the flow here.
+	fmt.Println("After mightPanic")
 }
 
 func main() {
-
+	// Because 'recoverable' caught the panic, the program doesn't crash.
+	// main() will finish gracefully.
 	recoverable()
-
+	fmt.Println("Main finished safely")
 }
